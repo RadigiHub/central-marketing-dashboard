7d8d14b43e6df94fe2a0004075d2c31ad9ede13b
RadigiHub <teamradmarketing@gmail.com>
2025-12-05 22:10:53 +0000

    Update team-updates.js

--- pages/team-updates.js
+++ pages/team-updates.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import supabase from "../lib/supabaseClient";
+import supabase from '../lib/supabase';
 import Layout from "../components/Layout";
 
 export default function TeamUpdates() {
