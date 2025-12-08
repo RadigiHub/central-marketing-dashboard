bd56e4646e4af29751867c6e9aec4ca51b905b30
RadigiHub <teamradmarketing@gmail.com>
2025-12-08 21:31:57 +0000

    Update task-assigned.js

--- pages/api/notify/task-assigned.js
+++ pages/api/notify/task-assigned.js
@@ -1,5 +1,5 @@
 // pages/api/notify-task.js
-import { sendTaskAssignedEmail } from "../../lib/notifications/email";
+import { sendTaskAssignedEmail } from "../../../lib/notifications/email";
 
 export default async function handler(req, res) {
   if (req.method !== "POST") {
